8c9d35c82e2bbf39de5b2fb8336e5aa4fc096576
Sholto Maud <sholto.maud@gmail.com>
2017-10-02 11:49:18 +1000

    up

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,21 @@
 
 FROM perl:latest
 
+RUN apt-get update && apt-get install words-\* -y
+
+ENV HOME=/home/app
+
 RUN curl -L http://cpanmin.us | perl - App::cpanminus
 RUN cpanm Carton Starman
+
+# COPY cpanfile cpanfile.snapshot $HOME/strat-dat/
 
 RUN cachebuster=907bdf2 git clone https://github.com/shotlom/strat-dat.git \
   && cd strat-dat \
   && carton install
 
+WORKDIR $HOME/strat-dat
+RUN carton install
+
 EXPOSE 8080
-WORKDIR strat-dat
 CMD carton exec starman --port 8080 bin/app.pl
--- lib/Strat/API.pm
+++ lib/Strat/API.pm
@@ -3,6 +3,7 @@ package Curry::Site;
 use Carp;
 use Dancer ':syntax';
 use Dancer::Plugin::Ajax;
+use Data::Dumper;
 use Moment;
 use JSON qw();
 
@@ -10,35 +11,82 @@ use lib::abs qw(
     ../../lib
 );
 
+my $wordlist = '/usr/share/dict/words';
+# open my $WORDS, '<', $wordlist or die "Cannot open $wordlist:$!";
+# chomp(my @words = <$WORDS>);
+# close $WORDS;
 
-hook 'before' => sub {
 
-    if ($ENV{TOKEN}) {
 
-        my $auth = request->{headers}->{authorization} // '';
-        $auth =~ /^TOKEN key="(.*?)"$/;
-        my $got_token = $1 // '';
-        print "got_token: [$got_token] ";
-
-        if ( $ENV{TOKEN} eq $got_token ) {
-            # auth is correct
-            print "auth is correct";
-        } else {
-            content_type('application/json');
-            print "auth is correct";
-            my $content = JSON::to_json({
-                success => JSON::false,
-                error_message => 'No access',
-            });
-
-            return halt($content);
-        }
-    }
-};
+#
+# hook 'before' => sub {
+#
+#     if ($ENV{TOKEN}) {
+#
+#         my $auth = request->{headers}->{authorization} // '';
+#         $auth =~ /^TOKEN key="(.*?)"$/;
+#         my $got_token = $1 // '';
+#         print "got_token: [$got_token] ";
+#
+#         if ( $ENV{TOKEN} eq $got_token ) {
+#             # auth is correct
+#             print "auth is correct";
+#         } else {
+#             content_type('application/json');
+#             print "auth is correct";
+#             my $content = JSON::to_json({
+#                 success => JSON::false,
+#                 error_message => 'No access',
+#             });
+#
+#             return halt($content);
+#         }
+#     }
+# };
 
 get '/' => sub {
-    return '<a href="https://github.com/bessarabov/curry">curry</a>';
+    return '
+      <div style="font-family:Arial; height:100%; padding:0; margin:0; display:-webkit-box; display:-moz-box; display:-ms-flexbox; display: -webkit-flex; display: flex; align-items: center; justify-content: center;">
+        <h3>Submission to strategicdata/recruitment</h3>
+        <a href="https://github.com/strategicdata/recruitment/wiki/Coding-task---BE">Coding task</a>
+        <h3>Response API</h3>
+        <ol type="1">
+          <li><a href="/wordfinder/dgo">Wordfinder API</a></li>
+          <li><a href="/ping">Ping</a></li>
+        </ol>
+        <h3>For local installation</h3>
+        <ol type="1">
+          <li>Download repo with `>git clone https://github.com/shotlom/strat-dat && cd strat-dat`</li>
+          <li>Ensure docker-compose is installed</li>
+          <li>Run `>docker-compose up`</li>
+        </ol>
+      </div>
+    ';
 };
+
+get '/wordfinder/:input' => sub {
+    my $letters = param('input');
+    my @matches;
+    open WORDS, '<', $wordlist or die "Cannot open $wordlist:$!";
+    while (<WORDS>) {
+        chomp;
+        push @matches, $_ if /^(?:([$letters])(?!.*\1))*$/g;
+    }
+
+    my $json = to_json(\@matches, {utf8 => 1, pretty => 1});
+    return "<div style=\"font-family:Arial; height: 100%;    padding: 0;    margin: 0;    display: -webkit-box;    display: -moz-box;    display: -ms-flexbox;    display: -webkit-flex;    display: flex;    align-items: center;    justify-content: center;\">$json</div>";
+};
+
+get '/ping' => sub {
+    return "<div style=\"font-family:Arial; height: 100%;    padding: 0;    margin: 0;    display: -webkit-box;    display: -moz-box;    display: -ms-flexbox;    display: -webkit-flex;    display: flex;    align-items: center;    justify-content: center;\">200 OK</div>";
+};
+
+
+get '/user/:name' => sub {
+    my $username = param('name');
+    return "Hello $username!";
+};
+
 
 =head2 /api/1/version
 
